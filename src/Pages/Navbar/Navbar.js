03051a44b44782bcd2f85385dd13c84c45cf2d85
saatvik-29 <121422100+saatvik-29@users.noreply.github.com>
2024-04-07 10:15:39 +0530

    new file made for logo

--- src/Pages/Navbar/Navbar.js
+++ src/Pages/Navbar/Navbar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { RiCustomerService2Line } from "react-icons/ri";
-import logo from "../../image/logo.png";
+import logo from "../../image/logo1.png";
 import { Link } from "react-router-dom";
 import { useAuth } from "../Plans/Auth/auth"; // Import the useAuth hook
 
